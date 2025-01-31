964aa8d9b2f571f49d8bb41e0d6c1b0d3a1f01e4
Chris Blume <christopher.blume@gmx.de>
2025-01-31 21:39:59 +0100

    footer adjust

--- src/app/layout/component/app.footer.ts
+++ src/app/layout/component/app.footer.ts
@@ -8,7 +8,6 @@ import { RouterLink } from '@angular/router';
     template: ` <div class="layout-footer">
         Crossfit Tracker by
         <a href="https://github.com/ChristopherBlume" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">CB</a>
-        <span routerLink="/old">old dashboard</span>
     </div>`
 })
 export class AppFooter {}
