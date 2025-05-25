2aa791187c47b66c0cdcc8084303deefcc8a5aaa
Chris Blume <christopher.blume@gmx.de>
2025-05-25 16:44:36 +0200

    import of pipe

---
+++ src/app/core/pipes/exercise-names.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ExerciseNamesPipe } from './exercise-names.pipe';
+
+describe('ExerciseNamesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ExerciseNamesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
