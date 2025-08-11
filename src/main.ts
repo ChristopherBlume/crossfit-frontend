06941294caf4fa119793600b7c7d42f65dd2d204
Chris Blume <christopher.blume@gmx.de>
2025-08-11 18:11:38 +0200

    pwa installation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,20 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app.config';
 import { AppComponent } from './app.component';
 import { MessageService } from 'primeng/api';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 const updatedAppConfig = {
     ...appConfig,
     providers: [
         ...(appConfig.providers || []), // Preserve existing providers from appConfig
-        MessageService
+        MessageService, provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          })
     ],
 };
 
